cd672667b357d54df66c8ed6e251313ca00c4552
savuskins@gmail.com <savuskins@gmail.com@02edc1a0-29d5-23ce-4551-22862a3cffd0>
2013-03-16 21:01:51 +0000

    fixed sql statement to show not useful states @ MisterX view

--- playground/functionality/move.php
+++ playground/functionality/move.php
@@ -3,7 +3,7 @@ session_start();
 $id =$_SESSION ['id'] ;
 include '../db_login_data.php';
 $id_player = $_SESSION['id_player'];
-
+$actor = $_SESSION["actor"];
 $ticket= $_POST ['ticket'];
 $value= $_POST ['value'];
 $coord= $_POST ['coord'];
@@ -14,5 +14,13 @@ if (! $con) {
 	echo ('Could not connect: ' . mysql_error ());
 }
 mysql_query("UPDATE ticket SET Value=".$value." WHERE SpielerID=".$id_player." AND Color='".$ticket."'");
-mysql_query("UPDATE Coord SET statusmrx=NULL WHERE statusmrx=1");
-mysql_query("UPDATE Coord SET statusmrx=1 WHERE CoordID=$coord");
+switch($actor) {
+    case 'MisterX' :
+        mysql_query("UPDATE Coord SET statusmrx=NULL WHERE statusmrx=1");
+        mysql_query("UPDATE Coord SET statusmrx=1 WHERE CoordID=$coord");
+    break;
+    case 'Detektiv' :
+        mysql_query("UPDATE Coord SET statusdet=NULL WHERE statusdet=1");
+        mysql_query("UPDATE Coord SET statusdet=1 WHERE CoordID=$coord");
+    break;
+}
